4d856edd52a6e0f23790089f760ed5c611d64ebf
Ketaki More <juliemore20012@gmail.com>
2025-11-11 17:06:43 +0530

    Refine base URL type declaration and default values for development and production environments.

--- baseUrl.ts
+++ baseUrl.ts
@@ -1,4 +1,4 @@
-export const baseURL =
+export const baseURL: string =
   process.env.NEXT_PUBLIC_BASE_URL || // Allow override for ngrok/tunnels
   (process.env.NODE_ENV == "development"
     ? "http://localhost:3000"
@@ -6,9 +6,9 @@ export const baseURL =
     ? // Netlify deployment
       (process.env.CONTEXT === "production"
         ? process.env.URL
-        : process.env.DEPLOY_URL)
+        : process.env.DEPLOY_URL) || "http://localhost:3000"
     : // Vercel deployment
       "https://" +
       (process.env.VERCEL_ENV === "production"
         ? process.env.VERCEL_PROJECT_PRODUCTION_URL
-        : process.env.VERCEL_BRANCH_URL || process.env.VERCEL_URL));
+        : process.env.VERCEL_BRANCH_URL || process.env.VERCEL_URL || "localhost:3000"));
