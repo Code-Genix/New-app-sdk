d2b5ceff1324080ed910fbd95a430018163118b5
Ketaki More <juliemore20012@gmail.com>
2025-11-11 16:56:16 +0530

    Enhance base URL configuration for deployment environments, adding support for Netlify and refining Vercel handling.

--- baseUrl.ts
+++ baseUrl.ts
@@ -2,7 +2,13 @@ export const baseURL =
   process.env.NEXT_PUBLIC_BASE_URL || // Allow override for ngrok/tunnels
   (process.env.NODE_ENV == "development"
     ? "http://localhost:3000"
-    : "https://" +
+    : process.env.NETLIFY
+    ? // Netlify deployment
+      (process.env.CONTEXT === "production"
+        ? process.env.URL
+        : process.env.DEPLOY_URL)
+    : // Vercel deployment
+      "https://" +
       (process.env.VERCEL_ENV === "production"
         ? process.env.VERCEL_PROJECT_PRODUCTION_URL
         : process.env.VERCEL_BRANCH_URL || process.env.VERCEL_URL));
